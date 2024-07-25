f1af197711ae61ae2a4d6c73c4c5f3618e16d6d8
Lukas <kowal.lukas88@gmail.com>
2024-07-25 14:56:25 +0100

    add carbon design

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
 "use client"
-import { useGetStarships } from "@/src/api/useGetStarships";
+import { useGetStarships } from "@/app/api/useGetStarships";
+import { Button } from 'carbon-components-react';
 
 export default function Home() {
 
   const { data, error, isLoading } = useGetStarships();
 
   const renderStarships = () => {
-    return data?.results.map(starship => <div>{starship.name}</div>)
+    return data?.results.map(starship => <div key={starship.MGLT} className="">{starship.name}</div>)
   }
-
 
   if (error) return <div>failed to load</div>
   if (isLoading) return <div>loading...</div>
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <Button>Example usage</Button>
+      <div className="flex flex-row">
         {renderStarships()}
       </div>
     </main>
