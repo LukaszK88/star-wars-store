82e1420f157a6455cc7ee0a7fe4fdc05fae0a4ac
Lukas <kowal.lukas88@gmail.com>
2024-07-25 19:07:56 +0100

    tidy up styling

--- app/components/starshipListProduct/StarshipListProduct.tsx
+++ app/components/starshipListProduct/StarshipListProduct.tsx
@@ -1,4 +1,4 @@
-import { Button, Tile, ButtonSet, Tag } from 'carbon-components-react';
+import { Button, Tile, NumberInput, Tag } from 'carbon-components-react';
 import {
   Add,
   Subtract,
@@ -28,16 +28,19 @@ export default function StarshipListProduct({ starship }: Props) {
 
   return (
     <Tile>
+
+
+        
       <div>
         <div>
           <h1 className={styles.name}>{starship.name}</h1>
           <h1 className={styles.model}>{starship.model}</h1>
         </div>
-        <div className={classNames(styles.price, 'pt-4')}>
+        <div className={styles.price}>
           {formatPrice(starship.cost_in_credits)}
         </div>
 
-        <div className={classNames(styles.overview, 'pt-4')}>Overview</div>
+        <div className={styles.overview}>Overview</div>
         <div className='grid grid-cols-3 gap-2'>
           <Tag type='outline' renderIcon={UserMultiple}>
             {starship.crew} Passengers
@@ -46,10 +49,10 @@ export default function StarshipListProduct({ starship }: Props) {
             {starship.crew} Crew
           </Tag>
           <Tag type='outline' renderIcon={Portfolio}>
-            {starship.cargo_capacity}{' '}
+            {starship.cargo_capacity}
           </Tag>
           <Tag type='outline' renderIcon={Wheat}>
-            {starship.consumables}{' '}
+            {starship.consumables}
           </Tag>
           <Tag type='outline' renderIcon={StarFilled}>
             {starship.hyperdrive_rating} Hyperdrive
@@ -64,35 +67,13 @@ export default function StarshipListProduct({ starship }: Props) {
             {starship.starship_class}
           </Tag>
         </div>
-      </div>
-
-      {/*       
-
-
-      // consumbales
-      // cargo
-      // crew
-      // hyperdrive rating
-      // model */}
-
-      <div className='flex flex-row justify-between'>
-        <div className='flex flex-row'>
-          <Button
-            kind='tertiary'
-            renderIcon={Add}
-            iconDescription='Add unit'
-            hasIconOnly
-          />
-          <div className='h-full px-6 bg-white text-center leading-10'>1</div>
-          <Button
-            kind='tertiary'
-            renderIcon={Subtract}
-            iconDescription='Remove unit'
-            hasIconOnly
-          />
+      
+      <div className='flex flex-row py-4'>
+        <div >
+          <NumberInput className='px-4' size='sm' id="number-input" label="Number of Ships" min={0} value={1} invalidText="Number is not valid" />
         </div>
-
-        <Button isExpressive>BUY</Button>
+        <Button className='items-center' isExpressive>BUY</Button>
+      </div>
       </div>
     </Tile>
   );
