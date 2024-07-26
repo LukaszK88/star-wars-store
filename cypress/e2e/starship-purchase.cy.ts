b2d740664d4fa0cbaec30e6647f517fe019328dd
Lukas <kowal.lukas88@gmail.com>
2024-07-26 11:42:37 +0100

    add e2e tests

---
+++ cypress/e2e/starship-purchase.cy.ts
@@ -0,0 +1,31 @@
+import { starshipResponse } from "../../app/tests/fixtures/starship"
+
+describe('starship purchase', () => {
+
+  before(() => {
+    cy.intercept('GET', 'https://swapi.dev/api/starships', {
+      statusCode: 200,
+      body: starshipResponse,
+    })
+
+  })
+
+  it('should add starship to basket', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.contains('Add to Basket').click()
+    
+    cy.contains(`Added 1 - ${starshipResponse.results[0].name} to basket`).should('be.visible')
+  })
+
+  it('should add multiple starships to basket', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.get('button[title="Increment number"]:first').click()
+    cy.get('button[title="Increment number"]:first').click()
+    
+    cy.contains('Add to Basket').click()
+
+    cy.contains(`Added 3 - ${starshipResponse.results[0].name} to basket`).should('be.visible')
+  })
+})
